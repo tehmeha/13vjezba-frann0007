4ed2284ce4011a661419f5b57e60b8f71a9a1cc8
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-16 16:36:44 +0200

    Create mini-banka.cpp

---
+++ mini-banka.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Glavni izbornik" << endl;
+    cout << "1. Unos nove osobe" << endl;
+    cout << "2. Ispisi podatke" << endl;
+    cout << "3. pretraga prema racunu" << endl;
+    cout << "4. Pretraga prema prezimenu" << endl;
+    cout << "5. Ispisi sortirano" << endl;
+    int izbor;
+    cin >> izbor;
+}
