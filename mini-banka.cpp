87b4ef11a3d50c35cfafd0cb22d902f48349b096
TommyLeeJones <antezelenika12@gmail.com>
2017-05-17 11:56:09 +0200

    korisnik unosi novu osobu

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -11,4 +11,18 @@ int main()
     cout << "5. Ispisi sortirano" << endl;
     int izbor;
     cin >> izbor;
+
+    unsigned long long ont brRacuna[50];
+    string prezimeIme[50];
+    float saldo[50];
+    int brojKlijenata = 0;
+    if ( izbor == 1 )
+    {
+        cout << "Unesite broj racuna : ";
+        cin >> brRacuna[brojKlijenata];
+        cout << "Unesite ime i prezime: ";
+        getline(cin, prezimeIme[brojKlijenata]);
+        saldo[brojKlijenata] = 0;
+        brojKlijenata ++;
+    }
 }
